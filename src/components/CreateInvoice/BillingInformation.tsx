14748cbbfe8bb48517d4b1ad6c30ea9ec67013c7
Osama Khalil <hey@ruzma.co>
2025-07-30 17:42:42 +0000

    Reverted to commit 460c8076ca894bf06fe0a9a09d1c0a1e8096b611

--- src/components/CreateInvoice/BillingInformation.tsx
+++ src/components/CreateInvoice/BillingInformation.tsx
@@ -57,7 +57,7 @@ const BillingInformation: React.FC<BillingInformationProps> = ({
             <div>
               <Select onValueChange={handleClientSelect}>
                 <SelectTrigger>
-                  <SelectValue placeholder="Select client" />
+                  <SelectValue />
                 </SelectTrigger>
                 <SelectContent>
                   {clients.map(client => (
@@ -73,13 +73,6 @@ const BillingInformation: React.FC<BillingInformationProps> = ({
                   </SelectItem>
                 </SelectContent>
               </Select>
-            </div>
-            <div>
-              <Input 
-                placeholder="Client name" 
-                value={invoiceData.billedTo.name} 
-                onChange={(e) => updateAddressField('billedTo', 'name', e.target.value)} 
-              />
             </div>
             <div>
               <Textarea 
